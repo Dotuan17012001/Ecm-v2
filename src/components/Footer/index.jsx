c1cc95e4c31032092e1982f594fb4e9f8ae14aa2
Dotuan17012001 <dovantuan17012001@gmail.com>
2023-10-06 13:59:40 +0700

    authenticated

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import './footer.scss';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>The world's first electronic trading website</div>
+        </footer>
+    )
+}
+
+export default Footer;
