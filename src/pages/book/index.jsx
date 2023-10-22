b41321e6389f70222be26e7059a71e16bed49005
Dotuan17012001 <dovantuan17012001@gmail.com>
2023-10-22 13:45:20 +0700

    URL, Param va Query

--- src/pages/book/index.jsx
+++ src/pages/book/index.jsx
@@ -1,4 +1,13 @@
+import { useLocation } from "react-router-dom";
+
 const BookPage = () => {
+    const location = useLocation()
+    console.log('locationCheck', location)
+
+    let param = new URLSearchParams(location.search);
+    console.log('checkParams',param)
+    const id = param?.get("id")
+    console.log('checkID',id)
     return (
         <>
             book page
